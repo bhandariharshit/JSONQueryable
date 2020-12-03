71f5b854f7080b1a1a65a901b9d346cfe0c5a2f3
Harshit <test@test.com>
2020-12-03 12:02:57 +0530

    Add project files.

---
+++ JSONQueryable/Predicate.cs
@@ -0,0 +1,97 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// This class only considers logical operation 'and' for the sake of POC and checks for following operators:
+/// >=
+/// <=
+/// >
+/// <
+/// =
+/// </summary>
+namespace JSONQueryable
+{
+    public static class Predicate
+    {
+        public static bool IsMatch(string jsonObject, string filter)
+        {
+            var jsonInput = JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonObject);
+            var filteredResults = new Dictionary<string, Object>();
+
+            string[] str = new string[] { "and" };
+
+            var splitResult = filter.Split(str, StringSplitOptions.None);
+            foreach (var item in splitResult)
+            {
+                var operatorResult = item.Split(new string[] { ">=" }, StringSplitOptions.None);
+                if (operatorResult.Count() > 1)
+                {
+                    filteredResults = jsonInput.Where(c => c.Key == operatorResult[0].Trim() && Convert.ToInt32(c.Value) >= Convert.ToInt32(operatorResult[1].Trim())).ToDictionary(c => c.Key, c => c.Value);
+                    if (jsonInput.Count == 0)
+                    {
+                        return false;
+                    }
+                    continue;
+                }
+
+                operatorResult = item.Split(new string[] { "<=" }, StringSplitOptions.None);
+                if (operatorResult.Count() > 1)
+                {
+                    filteredResults = jsonInput.Where(c => c.Key == operatorResult[0].Trim() && Convert.ToInt32(c.Value) <= Convert.ToInt32(operatorResult[1].Trim())).ToDictionary(c => c.Key, c => c.Value);
+                    if (filteredResults.Count == 0)
+                    {
+                        return false;
+                    }
+                    continue;
+                }
+
+                operatorResult = item.Split('>');
+                if (operatorResult.Count() > 1)
+                {
+                    filteredResults = jsonInput.Where(c => c.Key == operatorResult[0].Trim() && Convert.ToInt32(c.Value) > Convert.ToInt32(operatorResult[1].Trim())).ToDictionary(c => c.Key, c => c.Value);
+                    if (filteredResults.Count == 0)
+                    {
+                        return false;
+                    }
+                    continue;
+                }
+
+                operatorResult = item.Split('<');
+                if (operatorResult.Count() > 1)
+                {
+                    filteredResults = jsonInput.Where(c => c.Key == operatorResult[0].Trim() && Convert.ToInt32(c.Value) < Convert.ToInt32(operatorResult[1].Trim())).ToDictionary(c => c.Key, c => c.Value);
+                    if (filteredResults.Count == 0)
+                    {
+                        return false;
+                    }
+                    continue;
+                }
+
+                operatorResult = item.Split(new string[] { "=" }, StringSplitOptions.None);                
+                if (operatorResult.Count() > 1)
+                {
+                    if (operatorResult[1].Trim().StartsWith("'"))
+                    {
+                        filteredResults = jsonInput.Where(c => c.Key == operatorResult[0].Trim() && c.Value.ToString() == operatorResult[1].Trim(new char[] {' ', '\''})).ToDictionary(c => c.Key, c => c.Value);
+                    }
+                    else
+                    {
+                        filteredResults = jsonInput.Where(c => c.Key == operatorResult[0].Trim() && Convert.ToInt32(c.Value) == Convert.ToInt32(operatorResult[1].Trim())).ToDictionary(c => c.Key, c => c.Value);
+                    }
+
+                    if (filteredResults.Count == 0)
+                    {
+                        return false;
+                    }
+                }                
+            }
+
+            if (jsonInput.Count > 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ JSONQueryable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace JSONQueryable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filter = args[0];
+
+            string jsonObject = args[1];
+
+            bool isMatch = Predicate.IsMatch(jsonObject, filter);
+
+            Console.WriteLine(isMatch);
+            Console.ReadLine();     
+        }
+    }
+}
